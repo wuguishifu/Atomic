db38f1aff79d92af10eb7361df5a9709ef262d8e
Bo Bramer <bramer.bo@gmail.com>
2020-12-05 23:49:12 -0800

    Began working on molecule class.

--- src/com/bramerlabs/atomic/molecule/Molecule.java
+++ src/com/bramerlabs/atomic/molecule/Molecule.java
@@ -1,6 +1,11 @@
 package com.bramerlabs.atomic.molecule;
 
+import java.util.ArrayList;
+
 public class Molecule {
+
+    private int numAtoms;
+    private ArrayList<Atom> atoms;
 
     public Molecule() {
 
