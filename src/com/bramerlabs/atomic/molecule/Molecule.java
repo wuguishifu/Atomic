e98eb313a164cca40e4b04f238f33b7d212c41cc
Bo Bramer <bramer.bo@gmail.com>
2020-12-06 00:08:43 -0800

    Added constructors to Molecule class.

--- src/com/bramerlabs/atomic/molecule/Molecule.java
+++ src/com/bramerlabs/atomic/molecule/Molecule.java
@@ -10,18 +10,41 @@ public class Molecule {
     // the atoms in this atom
     private ArrayList<Atom> atoms;
 
-    // matrices containing the bonds and bond orders;
+    // a matrix representing the bond and bond orders
+    // 0's represent no bond present, 1s represent presence of bonds
+    // 1, 2, 3, represent single, double, and triple bonds, respectively
     private int[][] bonds;
-    private int[][] bondOrders;
+
+    // an ArrayList containing the positions of the
+    private ArrayList<float[]> positions;
+
+    /**
+     * constructor for specified list of atoms and bonds
+     * @param atoms - list of atoms
+     * @param bonds - a matrix representing the bonds in the molecule
+     */
+    public Molecule(ArrayList<Atom> atoms, int[][] bonds) {
+        this.atoms = atoms;
+        this.numAtoms = atoms.size();
+        this.bonds = bonds;
+    }
 
     /**
      * constructor for specified list of atoms
      * @param atoms - list of atoms
      */
     public Molecule(ArrayList<Atom> atoms) {
-        this.numAtoms = atoms.size();
         this.atoms = atoms;
-        this.bonds = new int[numAtoms][numAtoms];
+        this.numAtoms = atoms.size();
+        bonds = new int[numAtoms][numAtoms];
+
+        // fill the bonds matrix with 0's
+        for (int i = 0; i < numAtoms; i++) {
+            for (int j = 0; j < numAtoms; j++) {
+                bonds[i][j] = 0;
+            }
+        }
     }
+
 
 }
