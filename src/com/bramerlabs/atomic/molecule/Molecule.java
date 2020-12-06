5bfaf366c79897fdc8bbc76ba824eb1cd25628da
Bo Bramer <bramer.bo@gmail.com>
2020-12-05 23:51:26 -0800

    Began working on molecule class.

--- src/com/bramerlabs/atomic/molecule/Molecule.java
+++ src/com/bramerlabs/atomic/molecule/Molecule.java
@@ -4,11 +4,24 @@ import java.util.ArrayList;
 
 public class Molecule {
 
+    // the amount of atoms in this atom
     private int numAtoms;
+
+    // the atoms in this atom
     private ArrayList<Atom> atoms;
 
-    public Molecule() {
+    // matrices containing the bonds and bond orders;
+    private int[][] bonds;
+    private int[][] bondOrders;
 
+    /**
+     * constructor for specified list of atoms
+     * @param atoms - list of atoms
+     */
+    public Molecule(ArrayList<Atom> atoms) {
+        this.numAtoms = atoms.size();
+        this.atoms = atoms;
+        this.bonds = new int[numAtoms][numAtoms];
     }
 
 }
